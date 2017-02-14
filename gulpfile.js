0f76d3c190141e5b2cabf3a61b4b868cf1dc42c6
hemlok <adam.seckel@gmail.com>
2017-02-13 22:10:12 -0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+// Get one .styl file and render
+gulp.task('default', function () {
+  var processors = [
+    // autoprefixer(),
+    // autoprefixer({browsers: ['last 1 versions']}),
+    // cssnano(),
+  ];
+  return gulp.src('./tools.styl')
+    .pipe(stylus())
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./'));
+});
